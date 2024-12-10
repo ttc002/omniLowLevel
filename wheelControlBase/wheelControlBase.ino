9d00ffba4109d4e46c4e3f4119e560dca7b76eb2
ttc002 <shokalPetr@gmail.com>
2024-12-10 17:59:25 +0300

    .

--- wheelControlBase/wheelControlBase.ino
+++ wheelControlBase/wheelControlBase.ino
@@ -4,10 +4,10 @@ GStepper< STEPPER2WIRE> stepper1(800, 39, 33, 14);
 GStepper< STEPPER2WIRE> stepper2(800, 43, 37, 14);
 GStepper< STEPPER2WIRE> stepper3(800, 35, 41, 14);
 
-int speed1 = 0;    //speed for 1st stepper
-int speed2 = 0;    //speed for 2nd stepper
-int speed3 = 0;    //speed for 3rd stepper
-long time = 0;      //time, ms
+int speed1 = 33.95;    //speed for 1st stepper
+int speed2 = -46.38;    //speed for 2nd stepper
+int speed3 = 12.42;    //speed for 3rd stepper
+long time = 10;      //time, ms
 long timer = 0;
 void setup() 
 {
